35d59b6faf726b721e2f4e155e84d660d7871fea
IULIIA VOSKOBOINYK <juliia.whatever@gmail.com>
2025-02-03 21:24:40 +0100

    2

---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,32 @@
+import css from './Profile.module.css';
+
+function Profile({ name, tag, location, image, stats }) {
+  return (
+    <div className={css.profileCard}>
+      <div>
+        <img src={image} alt="User avatar" className={css.profileAvatar} />
+        <p className={css.profileName}>{name}</p>
+        <p className={css.profileTag}>{tag}</p>
+        <p className={css.profileLocation}>{location}</p>
+      </div>
+
+      <ul>
+        <li>
+          <span>Followers</span>
+          <span>{stats.followers}</span>
+        </li>
+
+        <li>
+          <span>Views</span>
+          <span>{stats.views}</span>
+        </li>
+
+        <li>
+          <span>Likes</span>
+          <span>{stats.likes}</span>
+        </li>
+      </ul>
+    </div>
+  );
+}
+export default Profile;
